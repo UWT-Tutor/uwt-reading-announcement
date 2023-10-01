1d244832ce4d144096bb8fdb6319d130e981de31
simple1017 <simple01281017@gmail.com>
2023-10-01 17:27:46 +0900

    feat: add Nav

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header className='fixed top-0 left-0 right-0 bg-primary w-full h-auto py-4 md:py-5 z-30'>
+       <div className="flex justify-between items-center container">
+        <h1 className='text-primary-foreground text-3xl'>UWT</h1>
+       </div>
+    </header>
+  )
+}
+
+export default Nav
